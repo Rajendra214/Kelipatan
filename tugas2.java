e067449f50793ec1d5c1e6d472ba77038f914c77
Rajendra214 <rajendraariellasatriani@gmail.com>
2024-11-04 15:37:49 +0700

    Add files via upload

---
+++ tugas2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author NITRO V 15
+ */
+public class tugas2 {
+ 
+    public static void main(String[] args) {
+        double kelipatan = 3.5;
+        for (double i = kelipatan; i <= 35; i += kelipatan) {
+            System.out.println(i);
+        }
+    }
+}
+
+    
+    
+
